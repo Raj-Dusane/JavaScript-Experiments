816965b7d5188076bd8201848312e243bb1e29f3
raj-dusane <dusaneraj15@gmail.com>
2024-09-22 14:27:34 +0530

    exp3 completed

--- exp3/script.js
+++ exp3/script.js
@@ -1,69 +1,179 @@
-// Function to replace characters using standard string methods
+// executing script only after the documents in completely loaded
+document.addEventListener('DOMContentLoaded', () => {
+  document.getElementById('inputString').addEventListener('input', validateInput);
+});
+
+function verifyOption() {
+  const e = document.getElementById('stringOperation');
+  const index = e.selectedIndex;
+  const errMsg = document.getElementById('invalidOption');
+  const nullDisplay = document.getElementById('display');
+  let result = document.getElementById('result');
+  
+  // display error msg for selecting invalid option
+  if (index==0){
+    nullDisplay.innerHTML = "";
+    result.innerHTML = "";
+    errMsg.innerHTML = `<p style="color: red; font-weight: bold;"> Select the correct option </p> <br>`;
+  }
+  else if (index==1){
+    errMsg.innerHTML = "";
+    nullDisplay.innerHTML = "";
+    result.innerHTML = "";
+    strReplacement();
+  }
+  else {
+    errMsg.innerHTML = "";
+    nullDisplay.innerHTML = "";
+    result.innerHTML = "";
+    palindrome();
+  }
+  return;
+}
+
+function strReplacement() {
+  const element = `
+  <fildset class="optionBox"></fildset>
+  <legend style="margin-bottom: 15px; font-weight: bold;">Select Method:</legend>
+  <div>
+  <label style="font-weight: normal;" for="standard">
+  <input type="radio" id="standard" name="standard" value="standard" selected> Standard Method
+  </label>
+  </div>
+  <div>
+  <label style="font-weight: normal;" for="nonStandard">
+  <input type="radio" id="nonStandard" name="standard" value="nonStandard"> Non Standard Method
+  </label>
+  </div>
+  <div id="unselectMethod"></div>
+  </fieldset>
+  
+  <div class="form-group">
+  <label for="charToReplace">Character to be replaced:</label>
+  <input type="text" id="charToReplace" placeholder="Enter character" autocomplete="off">
+  </div>
+  <div class="form-group">
+  <label for="replaceWith">Character to be replaced with:</label>
+  <input type="text" id="replaceWith" placeholder="Enter character" autocomplete="off">
+  </div>
+  
+  <div class="button">
+  <button onclick="replaceCharacters()">Replace</button>
+  </div>
+  `;
+  document.getElementById('display').innerHTML = element;  
+  return;
+}
+
+function validateInput() {
+  const inputStr = document.getElementById('inputString').value;
+  const errMsgElement = document.getElementById('errMsg');
+  const inputField = document.getElementById('inputString');
+  
+  if (!inputStr) {
+    inputField.style.borderColor = 'red';
+    errMsgElement.innerHTML = `<p style="color: red; font-weight: bold;"> Please enter a valid string </p>`;
+    return false;
+  } else {
+    inputField.style.borderColor = '';
+    errMsgElement.innerHTML = '';
+    return true;
+  }
+}
+
+function validateOptions() {
+  const selected = document.querySelector('input[name="standard"]:checked');
+  console.log(selected);
+  let errMsg = document.getElementById('unselectMethod');
+  if (!selected) {
+    errMsg.innerHTML=`<p style="color: red; font-weight: bold; margin: 15px 0 0 6px"> Please select an option </p>`;
+    return false;
+  }
+  else {
+    errMsg.innerHTML = '';
+    return {methodType : selected.value, execute: true};
+  }
+}
+
 function replaceCharacters() {
-    let input = document.getElementById('inputString').value;
-    const charToReplace = document.getElementById('replaceChar').value;
+  const selectedMethod = validateOptions();
   
-    // Validate if both input and charToReplace are provided
-    if (!input || !charToReplace) {
-      document.getElementById('result').innerText = `Please enter both a string and a character to replace.`;
-      return;
+  if (validateInput() && selectedMethod.execute){
+    const inputStr = document.getElementById('inputString').value;
+    const charToReplace = document.getElementById('charToReplace').value;
+    const symbol = document.getElementById('replaceWith').value;
+    let result = document.getElementById('result');
+
+    // Non-standard string methods
+    if (selectedMethod.methodType=="nonStandard") {
+      let replacedString = '';
+      for (let i = 0; i < inputStr.length; i++) {
+        if (inputStr[i] === charToReplace) {
+          replacedString += symbol;
+        } else {
+          replacedString += inputStr[i];
+        }
+        console.log(replacedString);
+      }
+      result.innerHTML = `<p> <span style="font-weight: bold;"> Output using non standard method: </span> <br> <br> ${replacedString} </p>`;
     }
-  
-    // Perform replacement using standard string methods
-    const replacedString = input.replace(new RegExp(charToReplace, 'g'), '@');
-    document.getElementById('result').innerText = `Replaced String (Using Methods): ${replacedString}`;
+
+    // standard string method
+    else {      
+      let replacedString = inputStr.replace(new RegExp(charToReplace, 'g'), symbol);
+      result.innerHTML = `<p> <span style="font-weight: bold"> Output using standard method: </span> <br> <br> ${replacedString} </p>`;
+    }
+  } 
+}
+
+// Palindrome Check
+function check(str){
+  let i=0, j=str.length-1;  
+  while (i<=j) {
+    if (str[i]!==str[j]){
+      return false;
+    }
+    else {
+      i++; j--; 
+    }
   }
-  
-  // Function to replace characters without using standard string methods
-  function replaceCharactersWithout() {
-    let input = document.getElementById('inputString').value;
-    const charToReplace = document.getElementById('replaceChar').value;
-  
-    // Validate if both input and charToReplace are provided
-    if (!input || !charToReplace) {
-      document.getElementById('result').innerText = `Please enter both a string and a character to replace.`;
-      return;
-    }
-  
-    // Perform replacement without standard string methods
-    let replacedString = '';
-    for (let i = 0; i < input.length; i++) {
-      if (input[i] === charToReplace) {
-        replacedString += '@';
-      } else {
-        replacedString += input[i];
+  return true;
+}
+
+function palindrome() {
+  if (validateInput()) {
+    const display = document.getElementById('display');
+    let result = document.getElementById('result');
+    
+    display.innerHTML = `
+      <div class="form-group" id="caseSense">
+      <label for="caseCheck">
+      <input type="checkbox" name="caseCheck" id="caseCheck" value="false" checked> Case Insensitive 
+      </label>
+      </div>
+    `;
+    
+    const caseCheck = document.getElementById('caseCheck');
+    
+    function updateResult() {
+      let inputStr = document.getElementById('inputString').value;
+
+      if (caseCheck.checked) {
+        let flag = check(inputStr.toLowerCase());
+        result.innerHTML = (flag === true) 
+          ? `<p style="font-weight: bold;"> Palindrome. </p>`
+          : `<p style="font-weight: bold;"> Not a palindrome. </p>`;
+      }
+
+      else {
+        let flag = check(inputStr);
+        result.innerHTML = (flag === true) 
+          ? `<p style="font-weight: bold;"> Palindrome. </p>`
+          : `<p style="font-weight: bold;"> Not a palindrome. </p>`;
       }
     }
-  
-    document.getElementById('result').innerText = `Replaced String (Without Methods): ${replacedString}`;
+    
+    document.getElementById('inputString').addEventListener('input', updateResult);
+    document.getElementById('caseCheck').addEventListener('change', updateResult);
   }
-  
-  // Function to check if a string is a palindrome
-  function checkPalindrome() {
-    const input = document.getElementById('palindromeString').value;
-  
-    // Validate if input is provided
-    if (!input) {
-      document.getElementById('result').innerText = `Please enter a string to check for palindrome.`;
-      return;
-    }
-  
-    // Normalize input by removing non-alphanumeric characters and converting to lowercase
-    const normalizedInput = input.toLowerCase().replace(/[^a-zA-Z0-9]/g, '');
-  
-    // Check for palindrome without using standard string methods
-    let isPalindrome = true;
-    for (let i = 0, j = normalizedInput.length - 1; i < j; i++, j--) {
-      if (normalizedInput[i] !== normalizedInput[j]) {
-        isPalindrome = false;
-        break;
-      }
-    }
-  
-    if (isPalindrome) {
-      document.getElementById('result').innerText = `"${input}" is a Palindrome.`;
-    } else {
-      document.getElementById('result').innerText = `"${input}" is not a Palindrome.`;
-    }
-  }
-  
+}
