6b20712a6717a90d50c913a8f06a4d1cee799a91
raj-dusane <dusaneraj15@gmail.com>
2024-09-22 14:27:07 +0530

    exp2 completed

--- exp2/script.js
+++ exp2/script.js
@@ -1,22 +1,53 @@
 function generateTable() {
     const number = parseInt(document.getElementById('number').value);
-    let table = '';
-
+    const multiplier = parseInt(document.getElementById('multiplier').value);
+    let forTable = '';
+    let doWhileTable = '';
+    let whileTable  = '';
+    
     if (isNaN(number)) {
         document.getElementById('tableResult').innerText = "Please enter a valid number.";
         return;
     }
 
-    table += '<ul>';
-    for (let i = 1; i <= 10; i++) {
-        table += `<li>${number} x ${i} = ${number * i}</li>`;
+    forTable += '<ul>';
+    for (let i = 1; i <= multiplier; i++) {
+        forTable += `<li>${number} x ${i} = ${number * i}</li>`;
     }
-    table += '</ul>';
+    forTable += '</ul>';
+    
+    doWhileTable += '<ul>';
+    let i=1;
+    do {
+        doWhileTable += `<li>${number} x ${i} = ${number * i}</li>`;
+        i++;
+    }
+    while (i<=multiplier);
+    doWhileTable += '</ul>';
 
-    let output = `
-                <br>
-                <h3>Multiplication Table for ${number} <\h3>
-                ${table} 
-                `
-    document.getElementById('tableResult').innerHTML = output;
+    whileTable += '<ul>';
+    i=1;
+    while (i <= multiplier) {
+        whileTable += `<li>${number} x ${i} = ${number * i}</li>`;
+        i++;
+    }
+    whileTable += '</ul>';
+    
+    let forLoopOutput = `
+        <h5> Using For loop </h5>
+        ${forTable} 
+    `
+    let doWhileLoopOutput = `
+        <h5> Using Do While loop </h5>
+        ${whileTable} 
+    `
+    let whileLoopOutput = `
+        <h5> Using While loop </h5>
+        ${doWhileTable} 
+    `
+    document.getElementById('heading').innerHTML = `<h3> Multiplication Table for ${number} <\h3>`;
+
+    document.getElementById('forLoop').innerHTML = forLoopOutput;
+    document.getElementById('doWhileLoop').innerHTML = doWhileLoopOutput;
+    document.getElementById('whileLoop').innerHTML = whileLoopOutput;
 }
