e6998a3dd9a34a503a9482d7a2822edcb4c3af2e
raj-dusane <dusaneraj15@gmail.com>
2024-09-25 22:40:08 +0530

    exp2 completed

--- exp2/script.js
+++ exp2/script.js
@@ -1,53 +1,76 @@
-function generateTable() {
+function redirectTo(method) {
     const number = parseInt(document.getElementById('number').value);
     const multiplier = parseInt(document.getElementById('multiplier').value);
-    let forTable = '';
-    let doWhileTable = '';
-    let whileTable  = '';
-    
-    if (isNaN(number)) {
-        document.getElementById('tableResult').innerText = "Please enter a valid number.";
+
+    if (isNaN(number) || isNaN(multiplier)) {
+        alert("Please enter a valid numbers.");
         return;
     }
 
-    forTable += '<ul>';
+    localStorage.setItem('number', number);
+    localStorage.setItem('multiplier', multiplier);
+
+    if (method === 'for') {
+        window.open('for.html', '_blank');
+    } else if (method === 'while') {
+        window.open('while.html', '_blank');
+    } else if (method === 'doWhile') {
+        window.open('dowhile.html', '_blank');
+    }
+}
+
+window.onload = function() {
+    const number = localStorage.getItem('number');
+    const multiplier = localStorage.getItem('multiplier');
+
+    if (number && multiplier) {
+        const pageTitle = document.title;
+        // if (pageTitle.includes('For Loop')) {
+        if (pageTitle === "Multiplication Table - For Loop") {
+            displayForLoop(parseInt(number), parseInt(multiplier));
+        } 
+        if (pageTitle === "Multiplication Table - Do While Loop") {
+            displayDoWhileLoop(parseInt(number), parseInt(multiplier));
+        } 
+        if (pageTitle === "Multiplication Table - While Loop") {
+            displayWhileLoop(parseInt(number), parseInt(multiplier));
+        }
+    }
+}
+
+function displayForLoop(number, multiplier) {
+    let forTable = '<ul>';
     for (let i = 1; i <= multiplier; i++) {
         forTable += `<li>${number} x ${i} = ${number * i}</li>`;
     }
     forTable += '</ul>';
     
-    doWhileTable += '<ul>';
-    let i=1;
-    do {
-        doWhileTable += `<li>${number} x ${i} = ${number * i}</li>`;
-        i++;
-    }
-    while (i<=multiplier);
-    doWhileTable += '</ul>';
+    document.getElementById('heading').innerText = `Multiplication Table for ${number}`;
+    document.getElementById('forLoop').innerHTML = forTable;
+}
 
-    whileTable += '<ul>';
-    i=1;
+function displayWhileLoop(number, multiplier) {
+    let whileTable = '<ul>';
+    let i = 1;
     while (i <= multiplier) {
         whileTable += `<li>${number} x ${i} = ${number * i}</li>`;
         i++;
     }
     whileTable += '</ul>';
     
-    let forLoopOutput = `
-        <h5> Using For loop </h5>
-        ${forTable} 
-    `
-    let doWhileLoopOutput = `
-        <h5> Using Do While loop </h5>
-        ${whileTable} 
-    `
-    let whileLoopOutput = `
-        <h5> Using While loop </h5>
-        ${doWhileTable} 
-    `
-    document.getElementById('heading').innerHTML = `<h3> Multiplication Table for ${number} <\h3>`;
+    document.getElementById('heading').innerText = `Multiplication Table for ${number}`;
+    document.getElementById('whileLoop').innerHTML = whileTable;
+}
 
-    document.getElementById('forLoop').innerHTML = forLoopOutput;
-    document.getElementById('doWhileLoop').innerHTML = doWhileLoopOutput;
-    document.getElementById('whileLoop').innerHTML = whileLoopOutput;
+function displayDoWhileLoop(number, multiplier) {
+    let doWhileTable = '<ul>';
+    let i = 1;
+    do {
+        doWhileTable += `<li>${number} x ${i} = ${number * i}</li>`;
+        i++;
+    } while (i <= multiplier);
+    doWhileTable += '</ul>';
+    
+    document.getElementById('heading').innerText = `Multiplication Table for ${number}`;
+    document.getElementById('doWhileLoop').innerHTML = doWhileTable;
 }
